b41880ffb6467294c97a0c1617f8ae0c1a333809
unknown <terkinasbox@gmail.com>
2023-05-16 13:06:34 +0300

    initial app

---
+++ src/components/Providers.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { FC, ReactNode } from 'react'
+import { SessionProvider } from 'next-auth/react'
+
+const Providers = ({ children }: { children: ReactNode }) => {
+    return (
+        <SessionProvider>
+            {children}
+        </SessionProvider>
+    )
+}
+
+export default Providers
---
+++ src/lib/auth.ts
@@ -0,0 +1,30 @@
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      }),
+    CredentialsProvider({
+      name: "Email",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = { id: "1", name: "Admin", email: "admin@admin.com" };
+        return user;
+      },
+    }),
+  ],
+};
