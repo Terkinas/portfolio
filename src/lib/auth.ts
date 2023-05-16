8fae0afe5d9d4e1d215adc5a89b8c069950e4f29
unknown <terkinasbox@gmail.com>
2023-05-16 13:18:31 +0300

    auth callbacks added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -27,4 +27,17 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl
+    },
+    async session({ session, user, token }) {
+      return session
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      return token
+    }}
 };
